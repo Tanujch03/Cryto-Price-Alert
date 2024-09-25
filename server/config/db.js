11c76e817fa7b2d390e85773809b0db2fd3dbfd2
Tanujch03 <tanujchaganti@gmail.com>
2024-09-25 23:30:19 +0530

    database connection added

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+// Function to connect to MongoDB
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+// Export the connectDB function
+module.exports = { connectDB };
